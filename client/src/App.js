22009293e47b5fb42985e7e1fb9e9a1d4b9be1f2
Basilio Farach <basiliofarach@pop-os.localdomain>
2021-08-24 23:45:20 -0600

    Test for modifying file and changing remote

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Fib Calculator
+            Fib Calculator V2
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
